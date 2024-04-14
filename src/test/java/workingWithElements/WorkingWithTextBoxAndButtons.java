aa0740a682c8af14d25c62691c4d58a287f9f152
muhamedalqemam <muhamed.alqemam@gmail.com>
2024-04-14 14:08:03 +0200

    E 113

---
+++ src/test/java/workingWithElements/WorkingWithTextBoxAndButtons.java
@@ -0,0 +1,53 @@
+package workingWithElements;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class WorkingWithTextBoxAndButtons {
+
+	FirefoxDriver driver;
+
+	@BeforeTest
+	public void OpenUrl() {
+		driver = new FirefoxDriver();
+		driver.navigate().to("http://the-internet.herokuapp.com/login");
+
+
+
+	}
+
+	@Test
+	public void FinfElementByClass () {
+		try {
+			WebElement TxtUserName = driver.findElement(By.id("username"));
+			WebElement TxtPassword = driver.findElement(By.id("password"));
+			WebElement BtnLogin = driver.findElement(By.className("radius"));
+			TxtUserName.clear();
+			TxtUserName.sendKeys("tomsmith");
+			TxtPassword.clear();
+			TxtPassword.sendKeys("SuperSecretPassword!");
+			BtnLogin.click();
+			assertEquals(driver.getCurrentUrl(), "http://the-internet.herokuapp.com/secure");
+
+
+		} catch (NoSuchElementException e) {
+			System.out.println("the element is not found.");
+		}
+
+
+	}
+
+	@AfterTest
+	public void CloseDriver() {
+		driver.close();
+	}
+
+
+}
